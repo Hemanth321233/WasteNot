bd754e01cd07bb45a65dd2cf8e95538239b85632
Narasimha Dasari <165119670+narasimhadasar@users.noreply.github.com>
2025-02-05 21:47:42 +0530

    Add files via upload

---
+++ Backend/types.ts
@@ -0,0 +1,7 @@
+// src/navigation/types.ts
+export type RootStackParamList = {
+    Auth: undefined; // No params needed for the Auth screen
+    Home: undefined; // No params needed for the Home screen
+    // Add other screens and their parameters here if needed
+  };
+  
